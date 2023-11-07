861e67c7d1a8f843e26d572413aa552d7906dc32
apreciado4 <apreciado4@live.com>
2023-11-07 15:01:13 -0800

    Website Skeleton

--- contact/forms.py
+++ contact/forms.py
@@ -3,26 +3,33 @@ from contact.models import ContactModel
 from django.utils.translation import gettext_lazy as _
 
 
-# class ContactForm(forms.Form):
-#     name = forms.CharField(required=True, max_length=25)
-#     email = forms.EmailField(required=True)
-#     phone_number = forms.CharField(required=True, max_length=10)
-#     message = forms.CharField(widget=forms.Textarea)
-
 class ContactForm(forms.ModelForm):
     class Meta:
         model = ContactModel
         fields = ('name', 'email', 'phone_number', 'subject', 'message')
+        labels = ({'name': 'Full Name',
+                   'email': 'Email Address',
+                   'phone_number': 'Phone Number',
+                   'subject': 'Subject',
+                   'message': 'Message'})
+
         widgets = {
-            'name': forms.TextInput(attrs={'placeholder': 'Full Name'}),
-            'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
-            'phone_number': forms.TextInput(attrs={'placeholder': 'Phone Number'}),
-            'subject': forms.TextInput(attrs={'placeholder': 'Subject'}),
+            'name': forms.TextInput(attrs={'placeholder': 'Full Name',
+                                           'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email',
+                                             'class': 'form-control'},),
+            'phone_number': forms.TextInput(attrs={'placeholder': 'Phone Number',
+                                                   'class': 'form-control'}),
+            'subject': forms.TextInput(attrs={'placeholder': 'Subject',
+                                              'class': 'form-control'}),
             'message': forms.Textarea(attrs={'placeholder': 'Enter Message Here',
-                                             'cols': 80, 'rows': 20}),
+                                             'cols': 80, 'rows': 20,
+                                             'class': 'form-control'}),
         }
+
         error_messages = {
             'name': {
                 'max_length': _('This name is too long'),
             },
+
         }
