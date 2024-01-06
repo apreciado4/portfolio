c9f825e8865461ba9c491a070406626f31974615
Andres Preciado <80650676+apreciado4@users.noreply.github.com>
2024-01-06 02:21:41 -0800

    azure 9 remove django-heroku settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -217,4 +217,4 @@ if SECURE_SSL_REDIRECT:
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Configure Django App for Heroku.
-django_on_heroku.settings(locals())
+# django_on_heroku.settings(locals())
