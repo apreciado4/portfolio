a4845475e01bf6c0d717c198b0e6efc38ef87b02
apreciado4 <apreciado4@live.com>
2024-01-15 20:44:26 -0800

    whitenoise mimetypes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -173,15 +173,17 @@ SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = 'app/static/'
+STATIC_ROOT = BASE_DIR/"staticfiles"
 
 # Add this in your settings.py file:
-STATICFILES_DIRS = [
-    BASE_DIR / "tmp/8dc16478a214895/static/"
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+WHITENOISE_MIMETYPES = {'.js': 'text/javascript'}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
